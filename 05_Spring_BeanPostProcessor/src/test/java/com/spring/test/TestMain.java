0b9a902f6fab84f8caab3dd4a32fb2829dafc207
vleuschen <1071309217@qq.com>
2019-12-19 23:56:20 +0800

    Spring的Bean后处理器，交给Spring容器管理的对象是代理对象

---
+++ 05_Spring_BeanPostProcessor/src/test/java/com/spring/test/TestMain.java
@@ -0,0 +1,28 @@
+package com.spring.test;
+
+import com.spring.beans.Teacher;
+import com.spring.service.BaseService;
+import com.spring.service.impl.SomeService;
+import com.spring.util.BeanDefined;
+import com.spring.util.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMain {
+
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+
+        ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("spring_config.xml");
+
+        BaseService serviceObj = (BaseService) factory.getBean("someService");
+
+        System.out.println(serviceObj.doSome());
+        
+
+
+    }
+
+}
